51abf79c3ab547b05e0283b653cb7e978a64fae0
steve.sturtevant <steve.sturtevant@appdynamics.com>
2015-05-28 14:51:23 -0500

    updated Payment Services

--- PayPalAuthService/src/main/java/com/appdynamics/sample/service/GenerateAccessToken.java
+++ PayPalAuthService/src/main/java/com/appdynamics/sample/service/GenerateAccessToken.java
@@ -1,10 +1,11 @@
 package com.appdynamics.sample.service;
 
 import com.paypal.base.ConfigManager;
+import com.paypal.base.Constants;
 import com.paypal.base.rest.OAuthTokenCredential;
 import com.paypal.base.rest.PayPalRESTException;
 
-public class GenerateAccessToken {
+public class GenerateAccessToken { 
 
 	public static String getAccessToken() throws PayPalRESTException {
 
@@ -12,10 +13,11 @@ public class GenerateAccessToken {
 		// Retrieve the access token from
 		// OAuthTokenCredential by passing in
 		// ClientID and ClientSecret
-		String clientID = ConfigManager.getInstance().getValue("clientID");
+		String clientID = ConfigManager.getInstance().getValue(Constants.CLIENT_ID);
 		String clientSecret = ConfigManager.getInstance().getValue(
-				"clientSecret");
+				Constants.CLIENT_SECRET);
 
-		return new OAuthTokenCredential(clientID, clientSecret).getAccessToken();
+		return new OAuthTokenCredential(clientID, clientSecret)
+				.getAccessToken();
 	}
 }
